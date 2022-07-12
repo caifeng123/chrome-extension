c2390a39e567a708eba2cb934551fb278058a065
caifeng01 <caifeng01@appledeMacBook-Pro.local>
2022-07-12 10:52:39 +0800

    feat: yarn 2.0

--- yarn/content.js
+++ yarn/content.js
@@ -1,25 +1,69 @@
-// 实际操作安装函数
-function addYarn(src, cb) {
-	const scrpit = document.createElement("script");
-	scrpit.src = src;
-	scrpit.onload = cb;
-	document.querySelector("body").append(scrpit);
+class CreateYarn {
+	#registerCDN = [];
+
+	constructor() {
+		// 记录已经安装的包
+		this.packages = [];
+		// 注册cdn函数列表，用作cdn挂时callback
+		this.#registerCDN = [this.#cdnjs, this.#jsdelivr];
+	}
+
+	// 注册cdn列表，竞态添加包
+	add = async packageName => {
+		try {
+			const detail = await Promise.any(this.#registerCDN.map(cdn => cdn(packageName)));
+			this.#addYarn(detail);
+		} catch (error) {
+			this.#fail(packageName);
+		}
+	}
+
+	// cdnjs方式
+	#cdnjs = async packageName => {
+		try {
+			const res = await fetch(`https://api.cdnjs.com/libraries/${packageName}`).then(res => res.json());
+			if (!res.error) {
+				return {src: res.latest, version: res.version, packageName, cdn: 'cdnjs'};
+			}
+			throw new Error();
+		} catch (error) {
+			throw new Error();
+		}
+	}
+
+	// jsdelivr方式
+	#jsdelivr = async packageName => {
+		try {
+			const tempUrl = `https://cdn.jsdelivr.net/npm/${packageName}`;
+			const access = await fetch(tempUrl);
+			if(access.ok) {
+				return {src: tempUrl, packageName, cdn: 'jsdelivr'};
+			}
+			throw new Error();
+		} catch (error) {
+			throw new Error();
+		}
+	}
+
+	// 报错提示
+	#fail = packageName => console.log(`用${this.#registerCDN.length}种方式加载, 发现${packageName}此包不存在, 请检查包名是否正确`);
+
+	// 实际操作安装函数
+	#addYarn = detail => {
+		const {src, version = '', packageName, cdn} = detail;
+
+		console.time("耗时");
+		console.log("加载中.... please wait a moment");
+		const scrpit = document.createElement("script");
+		scrpit.src = src;
+		scrpit.onload = () => {
+			const showPackageInfo = `cdn: ${cdn} - 加载了${packageName}${version ? `@${version}` : ''}`;
+			console.log(`加载完成✅ ${showPackageInfo}`);
+			console.timeEnd("耗时");
+			this.packages.push(detail);
+		};
+		document.body.appendChild(scrpit);
+	}
 }
 
-// 挂载安装 1、 查询 2、处理回调
-window.yarn = function (str) {
-	fetch(`https://api.cdnjs.com/libraries/${str}`)
-		.then((res) => res.json())
-		.then((res) => {
-			if (res.error) {
-				console.log(`${str}此包不存在 是不是打错了`);
-				return;
-			}
-			console.time("耗时");
-			console.log("加载中.... please wait a moment");
-			addYarn(res.latest, () => {
-				console.log(`加载完成✅ ${str}@${res.version}`);
-				console.timeEnd("耗时");
-			});
-		});
-};
+const yarn = new CreateYarn();
